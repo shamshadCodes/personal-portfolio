8b6e7593f94e1c04086b125c7116e25455734f2a
shamshad <lookatitonce@gmail.com>
2024-02-18 09:57:16 +0530

    added styles for Navbar

--- frontend-react/src/container/Navbar/Navbar.jsx
+++ frontend-react/src/container/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Navbar.scss";
 
+import { images } from "../../constants";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src="" alt="LOGO HERE" />
+      </div>
+      <ul className="app__navbar-links">
+        {["Home", "About", "Skills", "Work", "Contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
