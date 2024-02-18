c11d9300bfcdea949ceb4ee59d699ea0fd865d87
shamshad <lookatitonce@gmail.com>
2024-02-18 10:19:35 +0530

    added responsive style for smaller screens

--- frontend-react/src/container/Navbar/Navbar.jsx
+++ frontend-react/src/container/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import "./Navbar.scss";
+import React, { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 import { images } from "../../constants";
+import "./Navbar.scss";
 
 const Navbar = () => {
+  const [toggle, setToggle] = React.useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -17,6 +21,27 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {["Home", "About", "Skills", "Work", "Contact"].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
